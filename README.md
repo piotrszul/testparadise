fab4f5fcab19b394069fba8326a59d483317eb48
Piotr Szul <piotr.szul@csiro.au>
2019-03-15 11:10:45 +1000

    Working flask web service

---
+++ README.md
@@ -0,0 +1,26 @@
+
+
+CYPHER QUERIES
+
+	MATCH x=shortestPath((p)-[*]->(e))
+	WHERE ID(p)=60201 AND ID(e)=9
+	RETURN x
+
+
+
+## Notes
+
+- Embedded Neo4j: https://neo4j.com/docs/java-reference/3.5/tutorials-java-embedded/
+- Simple Service with python: https://nicolewhite.github.io/neo4j-flask/pages/register-a-user.html
+
+
+
+## Neo4j
+
+- Pyhon Driver: https://github.com/neo4j/neo4j-python-driver
+- Set Intial Password: https://neo4j.com/docs/operations-manual/current/configuration/set-initial-password/
+
+
+# Flash rest: 
+
+- https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
---
+++ python/service.py
@@ -0,0 +1,44 @@
+from flask import Flask, url_for, jsonify, abort
+from neo4j import GraphDatabase 
+
+app = Flask(__name__)
+
+driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "maggie26"))
+
+
+def path_to_ext(p):              
+    return [ node_to_ref(n) for n in p.nodes] 
+
+def node_to_ext(n):              
+    return dict(id = n.id, labels = list(n.labels), properties = dict(n))
+
+def node_to_ref(n):
+    return dict(id = n.id, labels = list(n.labels), name = n['name'],
+            uri=url_for('get_node', node_id = n.id, _external=True))
+
+@app.route('/')
+def index():
+    return 'index'
+
+
+@app.route('/node/<int:node_id>')
+def get_node(node_id):
+    with driver.session() as session:
+       result = session.run('MATCH (n) WHERE ID(n)=$node_id RETURN n',node_id=node_id).single()
+    if result is None:
+        abort(404)
+    else:
+        return jsonify(node_to_ext(result['n']))
+    
+
+@app.route('/shortestPath/<int:start_id>/<int:end_id>')
+def shortest_path(start_id, end_id):
+    with driver.session() as session:
+        result = session.run('MATCH path=shortestPath((b)-[*]-(e)) WHERE ID(b)=$start_id AND ID(e)=$end_id RETURN path', 
+                start_id=start_id, end_id=end_id).single()
+
+    if result is None:
+        abort(404)
+    else:
+        return jsonify({'shortestPath': path_to_ext(result['path'])})
+
