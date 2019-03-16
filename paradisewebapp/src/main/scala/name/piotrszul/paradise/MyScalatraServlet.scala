1ecc81264f8f0bed432c695bec8164fdf9e3c21d
Piotr Szul <piotr.szul@csiro.au>
2019-03-16 12:12:46 +1000

    Added json postprocessing and uri generation

--- paradisewebapp/src/main/scala/name/piotrszul/paradise/MyScalatraServlet.scala
+++ paradisewebapp/src/main/scala/name/piotrszul/paradise/MyScalatraServlet.scala
@@ -1,20 +1,39 @@
 package name.piotrszul.paradise
 
 import org.scalatra._
-import net.liftweb.json.JsonDSL._
-import net.liftweb.json.Serialization.{read, write}
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.json._
 import name.piotrszul.paradise.domain.Entity
+import org.json4s.JsonAST.JValue
+import org.json4s.ShortTypeHints
+import org.json4s.jackson.Serialization
+import org.json4s.JsonAST.JObject
+import org.json4s._
+import org.json4s.JsonDSL._
+  
+class MyScalatraServlet extends ScalatraServlet with JacksonJsonSupport with UrlGeneratorSupport {
 
-class MyScalatraServlet extends ScalatraServlet {
-
-  implicit val jsonFormat = net.liftweb.json.DefaultFormats
+  protected implicit lazy val jsonFormats: Formats = Serialization.formats(ShortTypeHints(List(classOf[Entity])))
+  
+  // Before every action runs, set the content type to be in JSON format.
+  before() {
+    contentType = formats("json")
+  }
   
   get("/") {
     Ok()
   }
 
-  get("/node/") {
-    Ok(write(Entity(10,"Piotr")))
+  val getNode = get("/node/:id") {
+    Ok(Entity(10,"Piotr"))
+  }
+
+  protected override def transformResponseBody(body: JValue): JValue = {
+    println(body)
+    val id = (body \ "id")
+    val asAsInt = id.extract[Int]
+    val uri = url(getNode,("id", asAsInt.toString))
+    body.removeField(f => f._1 == "jsonClass").asInstanceOf[JObject] ~ ("uri" -> uri)
   }
   
 }
--- paradisewebapp/src/main/scala/name/piotrszul/paradise/domain/Entity.scala
+++ paradisewebapp/src/main/scala/name/piotrszul/paradise/domain/Entity.scala
@@ -1,5 +1,7 @@
 package name.piotrszul.paradise.domain
 
+import org.codehaus.jackson.annotate.JsonTypeInfo
+
 case class Entity(id:Int, name:String) {
   
 }
